4709cf027b03b96f441d8c58408a92f44d490d8c
vivek devkar <mrvivekdevkar123@gmail.com>
2023-04-22 14:44:53 +0530

    deployment configuration

--- TnpMl/settings.py
+++ TnpMl/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-=)k%z8tw40nlxw1&1z%aqv6_vp&1$vduushc=galmn-c%_^qys
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1','*']
 
 
 # Application definition
@@ -117,7 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
